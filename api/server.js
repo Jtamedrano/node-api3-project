c17ec92e81df0e722c13b73c6379435d30ce6ae9
Jesse Medrano <Jtamedrano@gmail.com>
2021-04-06 22:59:25 -0700

    work in progress

--- api/server.js
+++ api/server.js
@@ -1,12 +1,18 @@
-const express = require('express');
+const express = require("express");
 
 const server = express();
 
 // remember express by default cannot parse JSON in request bodies
-
+const logger = (req, res, next) => {
+  console.log(req);
+  console.log(`[${req.method}]`);
+  next();
+};
 // global middlewares and the user's router need to be connected here
 
-server.get('/', (req, res) => {
+server.use(logger);
+
+server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
